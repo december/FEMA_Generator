0447156528ef4ae77f3330052f21ed8a59a70277
december <luyunfeicx@163.com>
2014-11-18 11:32:09 +0800

    The save operation for projection matrix added.

--- tucker.m
+++ tucker.m
@@ -1,4 +1,4 @@
-function [x, y, A1, A2, A3, name] = tucker(d, lg, lgc, mx, mn, t, dr, ndr, rate, noise, fileName)
+function [x, y, A1, A2, A3, name] = tucker(d, lg, lgc, mx, mn, t, dr, ndr, rate, noise, fileName, saveall)
 % d   : the number of dimension
 % lg  : length of the whole tensor
 % lgc : length of the core tensor
@@ -10,6 +10,7 @@ function [x, y, A1, A2, A3, name] = tucker(d, lg, lgc, mx, mn, t, dr, ndr, rate,
 % rate : the rate of non-zero nondiagonal elements in core tensor
 % noise : the max possible value of nondiagonal elements in core tensor
 % fileName : the file's name where the result saved
+% saveall : whether save all the infomation
 y = makecore(d, mx, mn, lgc, rate, noise)
 A1 = makepm(lg, lgc, t, dr, ndr)
 A2 = makepm(lg, lgc, t, dr, ndr)
@@ -18,5 +19,13 @@ A = {A1, A2, A3};
 TX = ttensor(y, A);
 x = tensor(TX)
 x_tenmat = tenmat(x, 1, 't');
-x_matrix = double(Y_tenmat);
+x_matrix = double(x_tenmat);
 dlmwrite(fileName, x_matrix);
+if saveall
+	dlmwrite(strcat(fileName, '_A1'), A1);
+	dlmwrite(strcat(fileName, '_A2'), A2);
+	dlmwrite(strcat(fileName, '_A3'), A3);
+	y_tenmat = tenmat(y, 1, 't');
+	y_matrix = double(y_tenmat);
+	dlmwrite(strcat(fileName, '_core'), y_matrix);
+end;
